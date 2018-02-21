d16b4cfedb417c2a3e4307097f4a0b5b34b2773d
Alex O. Holcombe <aoholcombe@gmail.com>
2018-02-21 17:27:56 +1100

    imported all sheets

--- importData.R
+++ importData.R
@@ -1,6 +1,6 @@
 #Import data
 
-#This way didn't work because complaints of Java out of memory
+#This way didn't work because complains of Java out of memory
 # dyn.load('/Library/Java/JavaVirtualMachines/jdk-9.0.4.jdk/Contents/Home/lib/server/libjvm.dylib') #modified from https://stackoverflow.com/questions/46366520/rjava-loading-error
 # require(rJava)
 # library(xlsx)
